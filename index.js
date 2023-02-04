5479efa44d667e2bcb67f826f906be6904b2c26c
Md. Tanvir Arif Siddiqui <tanvirtmm@gmail.com>
2023-02-04 21:34:44 +0600

    All Files are Uploaded

---
+++ index.js
@@ -0,0 +1,62 @@
+// Detecting Button press
+
+var buttonAll = document.querySelectorAll(".drum");
+for (var i = 0; i < buttonAll.length; i++) {
+  
+  buttonAll[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
+    // console.log(buttonInnerHTML);
+    playSound(buttonInnerHTML);
+    buttonAnnimation(buttonInnerHTML);
+  });
+}
+// Detecting Keyboaed Press
+
+  document.addEventListener("keypress", function (event) {
+    playSound(event.key);
+    buttonAnnimation(event.key);
+  });
+
+//Function
+function playSound(key){
+    switch (key) {
+      case "w":
+          var tom1 = new Audio("sounds/tom-1.mp3");
+          tom1.play();
+        break;
+      case "a":
+          var tom2 = new Audio("sounds/tom-2.mp3");
+          tom2.play();
+        break;
+      case "s":
+          var tom3 = new Audio("sounds/tom-3.mp3");
+          tom3.play();
+        break;
+      case "d":
+          var tom4 = new Audio("sounds/tom-4.mp3");
+          tom4.play();
+        break;
+      case "j":
+          var snare = new Audio("sounds/snare.mp3");
+          snare.play();
+        break;
+      case "k":
+          var crash = new Audio("sounds/crash.mp3");
+          crash.play();
+        break;
+      case "l":
+          var kick = new Audio("sounds/kick-bass.mp3");
+          kick.play();
+        break;
+      default:console.log(key);
+    }
+}
+
+function buttonAnnimation(currentKey){
+  var activeButton = document.querySelector("."+currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  },100);
+}
